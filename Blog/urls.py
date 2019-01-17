989f1d5ae7756fcb7ac443bdf7e6bdeb985222e8
Ace <michaelababao200@gmail.com>
2019-01-17 13:51:55 +0800

    Adding an base.html file so that you can use the same template over all of your files

--- Blog/urls.py
+++ Blog/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myblog.urls'))
+    path('myblog/', include('myblog.urls'))
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -8,6 +8,11 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.all().order_by('published_date')#filter(published_date__lte = timezone.now()).order_by('published_date')
     return render(request, 'myblog/post_list.html', {'posts':posts})
+
+def post_detail(request, id):
+    get_id = get_object_or_404(Post, id=id)
+    #get_id = Post.objects.all()
+    return render(request, 'myblog/post_detail.html', {'get_id':get_id})
 
 
 
